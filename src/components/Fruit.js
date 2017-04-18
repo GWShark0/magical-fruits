7228fa6f9503193d3f43b637b4488008ed5c1ffc
Greg Douglas <gwshark0@gmail.com>
2017-04-18 01:16:55 -0400

    Add select fruit logic

--- src/components/Fruit.js
+++ src/components/Fruit.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import classNames from 'classnames';
 import fruits from '../fruits';
 
 import './Fruit.css';
 
-function Fruit({ name }) {
+function Fruit({ name, selected, onClick }) {
+
+  const className = classNames(
+    'fruit',
+    { 'fruit--selected': selected },
+  );
+
   return (
-    <div className="fruit">
+    <div className={className} onClick={() => onClick(name)}>
       <img className="fruit__preview" src={fruits[name]} alt={name} />
     </div>
   )
